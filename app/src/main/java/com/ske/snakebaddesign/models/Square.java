a374c690cb0951acfbbe2c13e5ba126ecbab223a
Pipatpol Tanavongchinda <pipatpol.t@ku.th>
2016-03-17 12:37:05 +0700

    Add document

---
+++ app/src/main/java/com/ske/snakebaddesign/models/Square.java
@@ -0,0 +1,11 @@
+package com.ske.snakebaddesign.models;
+
+/**
+ * Created by Pipatpol on 2559-03-14.
+ */
+public class Square {
+    private int position;
+    public Square(int position){
+        this.position = position;
+    }
+}
